9503491d78767d7243a07b4d41356eab489e742a
jerry <jerry@usertesting.com>
2016-04-11 16:49:06 -0700

    The V2 API response does not include a "requestStatus" field

--- lib/voicebase/v2/response.rb
+++ lib/voicebase/v2/response.rb
@@ -1,6 +1,14 @@
 module VoiceBase
   module V2
     module Response
+
+      def success?
+
+        #todo the V2 API response does not include a "requestStatus" field
+        # need to determine Juergen's intent here.
+
+        ok? #&& request_status == "SUCCESS"
+      end
     end
   end
 end
