3d52f0a48983156325666021015c566b030baa9b
jerry <jerry@usertesting.com>
2016-04-14 15:35:03 -0700

    WIP clarify comments

--- lib/voicebase/v2/response.rb
+++ lib/voicebase/v2/response.rb
@@ -3,19 +3,27 @@ module VoiceBase
     module Response
 
       TRANSCRIPT_READY = "finished".freeze
+
       def success?
+
+        # for the V1 API this indicates both a successful HTTP status code and a values of "SUCCESS" in the
+        # returned JSON. with V2, there is no "SUCCESS" value. The combined use was split, adding
+        # #transcript_ready? to both interfaces.
+
         ok?
       end
 
       def transcript_ready?
-
         http_response.parsed_response['media'].first['status'].casecmp(TRANSCRIPT_READY) == 0
       end
+
+      #todo double check the format for plain text transcriptions, but it's probably just text only
       def transcript
         http_response.parsed_response['media'].first['transcripts']['latest']['words']
       end
 
       private
+
     end
   end
 end
