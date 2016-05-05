83ae434c92cabee1535dd4c29f056717fe84a031
jerry <jerry@usertesting.com>
2016-05-05 10:33:48 -0700

    fix broken spec

--- spec/lib/voicebase_spec.rb
+++ spec/lib/voicebase_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe VoiceBase do
   it "should have a version" do
-    expect(VoiceBase.version).to eq("1.0.14")
+    expect(VoiceBase.version).to eq("1.0.17")
   end
 end
